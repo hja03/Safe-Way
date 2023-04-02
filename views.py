60889f0bb475b56656ec13f19c09f46a818e927f
Joshua Peachey <jp2571@bath.ac.uk>
2023-04-02 08:38:33 +0100

    finished Navigation

--- views.py
+++ views.py
@@ -3,6 +3,13 @@ import folium
 import folium.plugins as plugins
 import pandas as pd
 import numpy as np
+from folium.plugins import PolyLineTextPath
+import numpy as np
+
+# Path finding functions
+# from JoshRouteFinding.getDirection import get_directions
+from JoshRouteFinding.getRoute import get_route
+# from JoshRouteFinding.locationAPICall import get_long_lat
 
 core = Blueprint('core', __name__, template_folder='templates')
 
@@ -33,7 +40,24 @@ def historical():
     br = (51.35, -2.329)
     folium.Rectangle([tl, br]).add_to(m)
 
-    locations = zip( applicable_rows['Latitude'], applicable_rows['Longitude'] )
+    locations = np.column_stack((applicable_rows['Latitude'], applicable_rows['Longitude']) )
+
+    if request.method == "POST":
+        user_input_start = request.form['from']
+        user_input_finish = request.form['to']
+        print("user Start", user_input_start)
+        print("user finish", user_input_finish)
+        starting_location, ending_location, route = get_route(user_input_start, user_input_finish, locations)
+        m = folium.Map(location=starting_location, zoom_start=12)
+        polyline = folium.PolyLine(locations=route, weight=5, color='blue')
+        polyline.add_to(m)
+        
+        folium.Marker(location=starting_location, popup='Start').add_to(m)
+        folium.Marker(location=ending_location, popup='End').add_to(m)
+        text = 'Route'
+        text_path = PolyLineTextPath(polyline, text)
+        text_path.add_to(m)
+
     plugins.HeatMap(locations, min_opacity=0.4, max_zoom=13).add_to(m)
 
     iframe = m.get_root()._repr_html_()
@@ -82,6 +106,22 @@ def ml():
     tl = (51.41, -2.428)
     br = (51.35, -2.329)
     folium.Rectangle([tl, br]).add_to(m)
+
+    # Plotting the route
+    if request.method == "POST":
+        user_input_start = request.form['from']
+        user_input_finish = request.form['to']
+        starting_location, ending_location, route = get_route(user_input_start, user_input_finish, locations)
+        m = folium.Map(location=starting_location, zoom_start=12)
+        polyline = folium.PolyLine(locations=route, weight=5, color='blue')
+        polyline.add_to(m)
+        
+        folium.Marker(location=starting_location, popup='Start').add_to(m)
+        folium.Marker(location=ending_location, popup='End').add_to(m)
+        text = 'Route'
+        text_path = PolyLineTextPath(polyline, text)
+        text_path.add_to(m)
+
 
     plugins.HeatMap(locations, min_opacity=0.4, max_zoom=13).add_to(m)
 
